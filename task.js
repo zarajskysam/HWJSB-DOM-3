467269019170d6d2a2997142e71ee2270c4a2086
Semyon Zarajsky <zarajskysam92@gmail.com>
2020-08-09 19:19:03 +0300

    correct added

--- task.js
+++ task.js
@@ -6,12 +6,18 @@ for (let i = 0; i < fontSizeButton.length; i++) {
         event.preventDefault();
         if (fontSizeButton[i].getAttribute("data-size") === "small") {
             removeClassBook();
+            removeButton();
+            fontSizeButton[i].classList.add("font-size_active");
             book.classList.add("book_fs-small");
         } else if (fontSizeButton[i].getAttribute("data-size") === "big") {
             removeClassBook();
+            removeButton();
+            fontSizeButton[i].classList.add("font-size_active");
             book.classList.add("book_fs-big");
         } else {
             removeClassBook();
+            removeButton();
+            fontSizeButton[i].classList.add("font-size_active");
         }
     })
 }
@@ -21,5 +27,12 @@ function removeClassBook() {
         book.classList.remove("book_fs-big");
     } else if (book.classList.contains("book_fs-small")) {
         book.classList.remove("book_fs-small");
+    }
+}
+function removeButton () {
+    for (let j = 0; j < fontSizeButton.length; j++){
+        if (fontSizeButton[j].classList.contains("font-size_active")){
+            fontSizeButton[j].classList.remove("font-size_active");
+        }
     }
 }
